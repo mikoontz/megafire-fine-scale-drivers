8e3813c3fef3da73ee44f8f680b326691c33b2d5
Michael Koontz <mikoontz@gmail.com>
2020-12-01 15:00:25 -0700

    proper s3 address

--- workflow/02_hourly-fire-progression/00_sync-goes-files-from-aws.R
+++ workflow/02_hourly-fire-progression/00_sync-goes-files-from-aws.R
@@ -5,8 +5,8 @@ dir.create("data/raw/goes16", recursive = TRUE, showWarnings = FALSE)
 dir.create("data/raw/goes17", recursive = TRUE, showWarnings = FALSE)
 
 (start <- Sys.time())
-system2(command = "aws", args = glue::glue("s3 sync noaa-goes16/ABI-L2-FDCC/ data/raw/goes16 --no-sign-request"), stdout = FALSE)
+system2(command = "aws", args = glue::glue("s3 sync s3://noaa-goes16/ABI-L2-FDCC/ data/raw/goes16 --no-sign-request"), stdout = FALSE)
 (difftime(Sys.time(), start, units = "mins"))
 
-system2(command = "aws", args = glue::glue("s3 sync noaa-goes17/ABI-L2-FDCC/ data/raw/goes17 --no-sign-request"), stdout = FALSE)
+system2(command = "aws", args = glue::glue("s3 sync s3://noaa-goes17/ABI-L2-FDCC/ data/raw/goes17 --no-sign-request"), stdout = FALSE)
 (difftime(Sys.time(), start, units = "mins"))
