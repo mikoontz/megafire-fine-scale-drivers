2208e629a5aa6a8ef200256f3fe9d1516290d090
Michael Koontz <mikoontz@gmail.com>
2020-12-01 20:57:31 -0700

    troubleshooting why so few rows in final dataframe.

--- workflow/02_hourly-fire-progression/01_ls-goes-files-from-aws.R
+++ workflow/02_hourly-fire-progression/01_ls-goes-files-from-aws.R
@@ -10,67 +10,66 @@ dir.create(here::here("data/out/"), recursive = TRUE, showWarnings = FALSE)
 dir.create(here::here("data/raw/"), recursive = TRUE, showWarnings = FALSE)
 
 ls_goes <- function(target_goes) {
-  if(!file.exists(here::here("data/out/goes_conus-filenames.csv"))) {
-    # GOES-16 record begins on 2017-05-24
-    
-    goes_raw_files <- 
-      list.files(glue::glue("data/raw/{target_goes}"), recursive = TRUE, full.names = TRUE)
-    
-    # bundle the list of filenames and extract some attributes from the
-    # metadata embedded in those filenames
-    goes_af <-
-      tibble::tibble(target_goes = target_goes,
-                     data_product = "ABI-L2-FDCC",
-                     local_path_full = goes_raw_files,
-                     aws_file = stringr::str_sub(string = local_path_full, start = 17, end = -1)) %>% 
-      tidyr::separate(col = aws_file, into = c("year", "doy", "hour", "filename"), sep = "/", remove = FALSE) %>% 
-      dplyr::mutate(filebasename = stringr::str_sub(string = filename, start = 1, end = -4)) %>% 
-      dplyr::mutate(doy = as.numeric(doy), year = as.numeric(year)) %>% 
-      dplyr::mutate(tmp_date = as.Date(doy, origin = glue::glue("{year}-01-01")),
-                    month = lubridate::month(tmp_date),
-                    day = lubridate::day(tmp_date)) %>% 
-      dplyr::mutate(scan_start = stringr::str_sub(filename, start = 24, end = 37),
-                    scan_end = stringr::str_sub(filename, start = 40, end = 53),
-                    scan_start_year = as.numeric(stringr::str_sub(scan_start, start = 1, end = 4)),
-                    scan_end_year = as.numeric(stringr::str_sub(scan_end, start = 1, end = 4)),
-                    scan_start_doy = as.numeric(stringr::str_sub(scan_start, start = 5, end = 7)),
-                    scan_end_doy = as.numeric(stringr::str_sub(scan_end, start = 5, end = 7)),
-                    scan_start_hour = as.numeric(stringr::str_sub(scan_start, start = 8, end = 9)),
-                    scan_end_hour = as.numeric(stringr::str_sub(scan_end, start = 8, end = 9)),
-                    scan_start_min = as.numeric(stringr::str_sub(scan_start, start = 10, end = 11)),
-                    scan_end_min = as.numeric(stringr::str_sub(scan_end, start = 10, end = 11)),
-                    scan_start_sec = as.numeric(stringr::str_sub(scan_start, start = 12, end = 14)) / 10,
-                    scan_end_sec = as.numeric(stringr::str_sub(scan_end, start = 12, end = 14)) / 10,
-                    scan_start_date = as.character(as.Date(scan_start_doy, origin = glue::glue("{scan_start_year}-01-01"))),
-                    scan_end_date = as.character(as.Date(scan_end_doy, origin = glue::glue("{scan_end_year}-01-01"))),
-                    scan_start_full = lubridate::ymd_hms(glue::glue("{scan_start_date} {scan_start_hour}:{scan_start_min}:{scan_start_sec}")),
-                    scan_end_full = lubridate::ymd_hms(glue::glue("{scan_end_date} {scan_end_hour}:{scan_end_min}:{scan_end_sec}")),
-                    scan_center_full = scan_start_full + difftime(scan_end_full, scan_start_full) / 2,
-                    scan_center_full = round(scan_center_full, units = "secs")) %>% # The midpoint between scan start and scan end; we'll use this to define a single scan time
-      dplyr::mutate(year = lubridate::year(scan_center_full),
-                    month = lubridate::month(scan_center_full),
-                    day = lubridate::day(scan_center_full),
-                    hour = lubridate::hour(scan_center_full),
-                    min = lubridate::minute(scan_center_full),
-                    sec = lubridate::second(scan_center_full),
-                    scan_center = paste0(year,
-                                         stringr::str_pad(string = month, width = 2, side = 'left', pad = '0'),
-                                         stringr::str_pad(string = day, width = 2, side = 'left', pad = '0'),
-                                         stringr::str_pad(string = hour, width = 2, side = 'left', pad = '0'),
-                                         stringr::str_pad(string = min, width = 2, side = 'left', pad = '0'),
-                                         stringr::str_pad(string = sec, width = 2, side = 'left', pad = '0')),
-                    processed_filename = glue::glue("{scan_center}_{filebasename}.csv")) %>% 
-      dplyr::select(target_goes, data_product, year, month, day, hour, min, sec, doy, filename, scan_start_full, scan_end_full, scan_center_full, scan_start, scan_end, scan_center, local_path_full, processed_filename, filebasename, aws_file)
-    
-  } # end if statement checking if file exists
+  goes_raw_files <- 
+    list.files(glue::glue("data/raw/{target_goes}"), recursive = TRUE, full.names = TRUE)
+  
+  # bundle the list of filenames and extract some attributes from the
+  # metadata embedded in those filenames
+  goes_af <-
+    tibble::tibble(target_goes = target_goes,
+                   data_product = "ABI-L2-FDCC",
+                   local_path_full = goes_raw_files,
+                   aws_file = stringr::str_sub(string = local_path_full, start = 17, end = -1)) %>% 
+    tidyr::separate(col = aws_file, into = c("year", "doy", "hour", "filename"), sep = "/", remove = FALSE) %>% 
+    dplyr::mutate(filebasename = stringr::str_sub(string = filename, start = 1, end = -4)) %>% 
+    dplyr::mutate(doy = as.numeric(doy), year = as.numeric(year)) %>% 
+    dplyr::mutate(tmp_date = as.Date(doy, origin = glue::glue("{year}-01-01")),
+                  month = lubridate::month(tmp_date),
+                  day = lubridate::day(tmp_date)) %>% 
+    dplyr::mutate(scan_start = stringr::str_sub(filename, start = 24, end = 37),
+                  scan_end = stringr::str_sub(filename, start = 40, end = 53),
+                  scan_start_year = as.numeric(stringr::str_sub(scan_start, start = 1, end = 4)),
+                  scan_end_year = as.numeric(stringr::str_sub(scan_end, start = 1, end = 4)),
+                  scan_start_doy = as.numeric(stringr::str_sub(scan_start, start = 5, end = 7)),
+                  scan_end_doy = as.numeric(stringr::str_sub(scan_end, start = 5, end = 7)),
+                  scan_start_hour = as.numeric(stringr::str_sub(scan_start, start = 8, end = 9)),
+                  scan_end_hour = as.numeric(stringr::str_sub(scan_end, start = 8, end = 9)),
+                  scan_start_min = as.numeric(stringr::str_sub(scan_start, start = 10, end = 11)),
+                  scan_end_min = as.numeric(stringr::str_sub(scan_end, start = 10, end = 11)),
+                  scan_start_sec = as.numeric(stringr::str_sub(scan_start, start = 12, end = 14)) / 10,
+                  scan_end_sec = as.numeric(stringr::str_sub(scan_end, start = 12, end = 14)) / 10,
+                  scan_start_date = as.character(as.Date(scan_start_doy, origin = glue::glue("{scan_start_year}-01-01"))),
+                  scan_end_date = as.character(as.Date(scan_end_doy, origin = glue::glue("{scan_end_year}-01-01"))),
+                  scan_start_full = lubridate::ymd_hms(glue::glue("{scan_start_date} {scan_start_hour}:{scan_start_min}:{scan_start_sec}")),
+                  scan_end_full = lubridate::ymd_hms(glue::glue("{scan_end_date} {scan_end_hour}:{scan_end_min}:{scan_end_sec}")),
+                  scan_center_full = scan_start_full + difftime(scan_end_full, scan_start_full) / 2,
+                  scan_center_full = round(scan_center_full, units = "secs")) %>% # The midpoint between scan start and scan end; we'll use this to define a single scan time
+    dplyr::mutate(year = lubridate::year(scan_center_full),
+                  month = lubridate::month(scan_center_full),
+                  day = lubridate::day(scan_center_full),
+                  hour = lubridate::hour(scan_center_full),
+                  min = lubridate::minute(scan_center_full),
+                  sec = lubridate::second(scan_center_full),
+                  scan_center = paste0(year,
+                                       stringr::str_pad(string = month, width = 2, side = 'left', pad = '0'),
+                                       stringr::str_pad(string = day, width = 2, side = 'left', pad = '0'),
+                                       stringr::str_pad(string = hour, width = 2, side = 'left', pad = '0'),
+                                       stringr::str_pad(string = min, width = 2, side = 'left', pad = '0'),
+                                       stringr::str_pad(string = sec, width = 2, side = 'left', pad = '0')),
+                  processed_filename = glue::glue("{scan_center}_{filebasename}.csv")) %>% 
+    dplyr::select(target_goes, data_product, year, month, day, hour, min, sec, doy, filename, scan_start_full, scan_end_full, scan_center_full, scan_start, scan_end, scan_center, local_path_full, processed_filename, filebasename, aws_file)
+  
+  return(goes_af)
 } # end function
 
 target_goes <- c("goes16", "goes17")
 
-goes_meta <- purrr::map_dfr(target_goes, .f = ls_goes)
-
-readr::write_csv(x = goes_meta, file = here::here("data/out/goes_conus-filenames.csv"))
-
+if(!file.exists(here::here("data/out/goes_conus-filenames.csv"))) {
+  # GOES-16 record begins on 2017-05-24
+  goes_meta <- purrr::map_dfr(target_goes, .f = ls_goes)
+  
+  readr::write_csv(x = goes_meta, file = here::here("data/out/goes_conus-filenames.csv"))
+}
 ### write the Mask and DQF meanings to disk
 
 # GOES-16 and GOES-17
@@ -91,7 +90,7 @@ readr::write_csv(x = goes_meta, file = here::here("data/out/goes_conus-filenames
 ###
 
 if(!file.exists(here::here("data/out/goes-mask-meanings.csv")) | !file.exists(here::here("data/out/goes-dqf-meanings.csv"))) {
-
+  
   # Get example .nc file
   ex_local_path_full <- goes_meta$local_path_full[1]
   
