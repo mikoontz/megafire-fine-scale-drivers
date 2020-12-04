6b55c34711843506f980c35f7e2073db08a0c96e
Michael Koontz <mikoontz@gmail.com>
2020-12-04 15:18:29 -0700

    source in external files in workflow

--- workflow/02_hourly-fire-progression/02_process-california-goes.R
+++ workflow/02_hourly-fire-progression/02_process-california-goes.R
@@ -12,6 +12,9 @@ if(!require(USAboundariesData)) {
 
 require(USAboundariesData)
 
+source("workflow/02_hourly-fire-progression/00_sync-goes-files-from-aws.R")
+source("workflow/02_hourly-fire-progression/01_ls-goes-files-from-aws.R")
+
 dir.create(here::here("data/out/goes/california"), recursive = TRUE, showWarnings = FALSE)
 
 if(!file.exists(here::here("data/out/goes_conus-filenames.csv")) | !file.exists(here::here("data/out/goes-mask-meanings.csv")) | !file.exists(here::here("data/out/goes-dqf-meanings.csv"))) {
@@ -26,8 +29,7 @@ if(!file.exists(here::here("data/out/goes_conus-filenames.csv")) | !file.exists(
 # Read in the GOES metadata acquired from Amazon Earth using 01_ls-goes-files-from-aws.R script
 goes_meta <- readr::read_csv(file = here::here("data/out/goes_conus-filenames.csv"), col_types = "cciiiiinicTTTccccccc")
 
-dir.create(here::here("data/raw/goes/california"), 
-           showWarnings = FALSE, recursive = TRUE)
+# dir.create(here::here("data/raw/goes/california"), showWarnings = FALSE, recursive = TRUE)
 
 california_geom <- USAboundaries::us_states(resolution = "high", states = "California")
 
@@ -39,8 +41,10 @@ fire_flags <-
 
 expected_cols <- c("x", "y", "Area", "Temp", "Mask", "Power", "DQF", "cell")
 
-subset_goes_to_california <- function(local_path_full, processed_name, ...) {
+subset_goes_to_california <- function(local_path_full, processed_filename, ...) {
   this <- stars::read_stars(here::here(local_path_full))
+  
+  this_crs <- sf::st_crs(this)$wkt
   
   ca_goes_geom <- 
     sf::st_transform(california_geom, crs = sf::st_crs(this))
@@ -60,7 +64,7 @@ subset_goes_to_california <- function(local_path_full, processed_name, ...) {
   
   this_ca <-
     this_ca %>% 
-    dplyr::select(expected_cols) %>%  # convert to data frame
+    dplyr::select(dplyr::all_of(expected_cols)) %>%  # convert to data frame
     dplyr::filter(!is.na(Mask)) %>%  # filter out all of the masked cells
     dplyr::filter(Mask %in% fire_flags) %>% # filter to just fire pixels
     sf::st_as_sf(coords = c("x", "y"), crs = sf::st_crs(this), remove = FALSE) %>% 
@@ -71,12 +75,14 @@ subset_goes_to_california <- function(local_path_full, processed_name, ...) {
   
   readr::write_csv(x = this_ca, file = glue::glue("{here::here()}/data/out/goes/california/{processed_filename}"))
   
-  rm(this)
-  rm(this_ca)
-  rm(ca_goes_geom)
-  gc()
+  crs_df <- data.frame(local_path_full, processed_filename, crs = this_crs)
   
-  return(NULL)
+  # rm(this)
+  # rm(this_ca)
+  # rm(ca_goes_geom)
+  # gc()
+  
+  return(crs_df)
 }
 
 # processed_goes <-
@@ -91,21 +97,30 @@ n_workers <- 48
 
 goes_meta_with_crs_batches <-
   goes_meta %>% 
-  # dplyr::filter(!(processed_name %in% processed_goes$filename)) %>%
+  # dplyr::filter(!(processed_filename %in% processed_goes$filename)) %>%
   dplyr::mutate(group = sample(x = 1:n_workers, size = nrow(.), replace = TRUE)) %>% 
   dplyr::group_by(group) %>% 
   dplyr::group_split()
 
 # remove all variables no longer needed (in case they get copied over to each core?)
-rm(processed_goes)
-rm(goes_meta)
+# rm(processed_goes)
+# rm(goes_meta)
 
 (start <- Sys.time())
 future::plan(strategy = "multiprocess", workers = n_workers)
 
-furrr::future_walk(goes_meta_with_crs_batches, .f = function(x) {
-  purrr::pwalk(.l = x, .f = subset_goes_to_california)
+goes_with_crs <-
+  furrr::future_map_dfr(goes_meta_with_crs_batches, .f = function(x) {
+  purrr::pmap_dfr(.l = x, .f = subset_goes_to_california)
 })
+
+system2(command = "aws", args = glue::glue("s3 sync {here::here()}data/out/goes/california s3://earthlab-mkoontz/megafire-fine-scale-drivers"), stdout = FALSE)
+
+
+# goes_with_crs <-
+#   furrr::future_map_dfr(goes_meta_with_crs_batches, .f = function(x) {
+#     purrr::pmap_dfr(.l = x, .f = get_goes_crs)
+#   })
 
 future::plan(strategy = "sequential")
 
