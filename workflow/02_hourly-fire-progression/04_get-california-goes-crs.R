f69243ebaae3cd7a96d9f2b3af95d2d0d78ff61e
Michael Koontz <mikoontz@gmail.com>
2021-01-12 11:45:55 -0700

    old california code

--- workflow/02_hourly-fire-progression/04_get-california-goes-crs.R
+++ workflow/02_hourly-fire-progression/04_get-california-goes-crs.R
@@ -14,5 +14,41 @@ if(!require(USAboundariesData)) {
 
 require(USAboundariesData)
 
-# Option 1 is to download the whole GOES record to local machine first
-# system2(command = "aws", args = glue::glue("s3 sync s3://earthlab-mkoontz/megafire-fine-scale-drivers/goes_california data/out/goes/california"))
+get_goes_crs <- function(local_path_full, processed_filename, ...) {
+  this <- stars::read_stars(here::here(local_path_full))
+  this_crs <- sf::st_crs(this)$wkt
+  
+  crs_df <- data.frame(local_path_full, processed_filename, crs = this_crs)
+  # ca_goes_geom <- 
+  #   sf::st_transform(california_geom, crs = sf::st_crs(this))
+  
+  # readr::write_csv(x = this_ca, file = glue::glue("{here::here()}/data/out/goes/california/{processed_filename}"))
+  # 
+  # rm(this)
+  # rm(this_ca)
+  # rm(ca_goes_geom)
+  # gc()
+  
+  return(crs_df)
+}
+
+n_workers <- 7
+
+goes_meta_with_crs_batches <-
+  goes_meta %>% 
+  # dplyr::filter(!(processed_filename %in% processed_goes$filename)) %>%
+  dplyr::mutate(group = sample(x = 1:n_workers, size = nrow(.), replace = TRUE)) %>% 
+  dplyr::group_by(group) %>% 
+  dplyr::group_split()
+
+(start <- Sys.time())
+future::plan(strategy = "multiprocess", workers = n_workers)
+
+goes_with_crs <-
+  furrr::future_map_dfr(goes_meta_with_crs_batches, .f = function(x) {
+  purrr::pmap_dfr(.l = x, .f = get_goes_crs)
+})
+
+future::plan(strategy = "sequential")
+
+(difftime(Sys.time(), start))
